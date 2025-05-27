63ce3e87589f238caf510e621b39018320d7cdd3
sneko <supernekocat31@gmail.com>
2025-05-28 00:30:23 +0900

    prac: add 06_composition_function

---
+++ 06_composition_function/app/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+f :: Int -> Int
+f n = n * 2
+
+g :: Int -> Int
+g n = n * 3
+
+h :: Int -> Int
+h n = n * 4
+
+compositionFunc :: Int -> Int
+compositionFunc = f . g . h  -- f(g(h(n)))
+
+main :: IO ()
+main = print $ compositionFunc 5
+
