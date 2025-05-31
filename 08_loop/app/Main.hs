a3f6f19e4cb374c4b982611b4eb8c5884d85efac
sneko <supernekocat31@gmail.com>
2025-05-31 12:13:52 +0900

    prac: add 08_loop

---
+++ 08_loop/app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+loop :: Int -> IO () -> IO ()
+loop 0 action = return ()
+loop n action = do 
+    action
+    loop (n - 1) action
+
+main :: IO ()
+main = loop 10 $ putStrLn "Hello"
+
