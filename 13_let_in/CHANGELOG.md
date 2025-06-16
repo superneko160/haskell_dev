010454d12202325eceed58c9ace43a708faa64dc
sneko <supernekocat31@gmail.com>
2025-06-17 00:41:42 +0900

    prac: add 13_let_in

---
+++ 13_let_in/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for x13-let-in
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ 13_let_in/app/Main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+{-
+    let bindings in expression
+-}
+
+-- 円柱の面積を求める
+cylinder :: Double -> Double -> Double
+cylinder r h = 
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+main :: IO ()
+main = print (cylinder 2.4 15.2)
+
