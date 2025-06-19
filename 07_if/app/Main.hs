afd9692707987fd5eea591f29d38da18013b6b79
sneko <supernekocat31@gmail.com>
2025-06-19 12:48:16 +0900

    add explanation in 07_if

--- 07_if/app/Main.hs
+++ 07_if/app/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+{-
+ - Haskellのifは式であり、文ではない
+ - つまり、Hakellのifは値を返す
+ - （下だとZeroかNotZeroを返す）
+ -
+ - また、ifは式なのでthenとelseで返す値は同じ型でなくてはならない
+ - （下だとZeroもNotZeroもString）
+ -}
 isZero :: Int -> String
 isZero x =
     if x == 0 then
